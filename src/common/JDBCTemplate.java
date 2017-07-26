18bf33140092623597167244773e7afcae690aeb
SBS <sbs12481@gmail.com>
2017-07-26 15:17:16 +0900

    디비연결시도1

--- src/common/JDBCTemplate.java
+++ src/common/JDBCTemplate.java
@@ -1,6 +1,29 @@
 package common;
 
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+
 public class JDBCTemplate 
 {
-
+	public static Connection getConnection()
+	{
+		Connection con=null;
+		Properties prop=new Properties();
+		
+		try 
+		{
+			String path=JDBCTemplate.class.getResource(".").getPath()+"driver.properties";
+			prop.load(new FileReader(path));
+			
+			Class.forName(prop.getProperty("driver"));
+			con=DriverManager.getConnection(prop.getProperty("url"),prop.getProperty("user"),prop.getProperty("password"));			
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}		
+		
+		return con;
+	}
 }
