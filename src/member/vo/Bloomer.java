db08c035d3f6652a13ae4b088ceb33f20ec984e5
SBS <sbs12481@gmail.com>
2017-07-27 18:08:14 +0900

    멤버 DAO 시작

--- src/member/vo/Bloomer.java
+++ src/member/vo/Bloomer.java
@@ -2,100 +2,26 @@ package member.vo;
 
 import java.sql.Date;
 
-public class Bloomer
+public class Bloomer extends Member
 {
-	private String mbcode;
-	private String  id;
-	private String  pwd;
-	private String  nick;
-	private String  email;
-	private String  phone;
 	private String  leader;
-	private Date enrollDay;
-	private Date leaveDay;
 	
 	public Bloomer(){}
 	
 	public Bloomer(String mbcode, String id, String pwd, String nick, String email, String phone, String leader,
-			Date enrollDay)
+			Date enrollDay,Date leaveDay,String category1, String category2, String category3)
 	{
-		super();
-		this.mbcode = mbcode;
-		this.id = id;
-		this.pwd = pwd;
-		this.nick = nick;
-		this.email = email;
-		this.phone = phone;
+		super(mbcode,id,pwd,nick,email,phone,enrollDay,leaveDay,category1,category2,category3);
 		this.leader = leader;
-		this.enrollDay = enrollDay;
 	}
-
+	
+	
 	@Override
 	public String toString()
 	{
-		return "Bloomer [mbcode=" + mbcode + ", id=" + id + ", pwd=" + pwd + ", nick=" + nick + ", email=" + email
-				+ ", phone=" + phone + ", leader=" + leader + ", enrollDay=" + enrollDay + ", leaveDay=" + leaveDay
-				+ "]";
-	}
-
-	public String getMbcode() 
-	{
-		return mbcode;
-	}
-
-	public void setMbcode(String mbcode) 
-	{
-		this.mbcode = mbcode;
-	}
-
-	public String getId() 
-	{
-		return id;
-	}
-
-	public void setId(String id) 
-	{
-		this.id = id;
-	}
-
-	public String getPwd() 
-	{
-		return pwd;
-	}
-
-	public void setPwd(String pwd) 
-	{
-		this.pwd = pwd;
-	}
-
-	public String getNick() 
-	{
-		return nick;
-	}
-
-	public void setNick(String nick) 
-	{
-		this.nick = nick;
-	}
-
-	public String getEmail() 
-	{
-		return email;
-	}
-
-	public void setEmail(String email) 
-	{
-		this.email = email;
-	}
-
-	public String getPhone() 
-	{
-		return phone;
-	}
-
-	public void setPhone(String phone) 
-	{
-		this.phone = phone;
+		return "Bloomer [mbcode=" + this.getMbcode() + ", id=" + this.getId() + ", pwd=" + this.getPwd() + ", nick=" + this.getNick() + 
+				", email=" + this.getEmail()+ ", phone=" + this.getPhone() + ", leader=" + this.getLeader() + ", enrollDay=" + this.getEnrollDay() +
+				", leaveDay=" + this.getLeaveDay()+ "]";
 	}
 
 	public String getLeader() 
@@ -106,25 +32,5 @@ public class Bloomer
 	public void setLeader(String leader) 
 	{
 		this.leader = leader;
-	}
-
-	public Date getEnrollDay() 
-	{
-		return enrollDay;
-	}
-
-	public void setEnrollDay(Date enrollDay) 
-	{
-		this.enrollDay = enrollDay;
-	}
-
-	public Date getLeaveDay() 
-	{
-		return leaveDay;
-	}
-
-	public void setLeaveDay(Date leaveDay)
-	{
-		this.leaveDay = leaveDay;
 	}
 }
